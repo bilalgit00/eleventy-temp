c9825aa9fd4c0f92003d532fb89b20fdf666a404
Bilal Hussain <bilalhussaintools@gmail.com>
2025-07-26 23:31:35 +0500

    Ensure admin folder is copied during build

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+  // Copy admin folder
+  eleventyConfig.addPassthroughCopy("admin");
+  
+  // Copy other assets
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/css");
+  
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  };
+};
